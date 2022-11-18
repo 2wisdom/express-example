d1758e3463fe90f81a0ea7a5f528f51cc4672d80
Lizzie <leejihye7117@gmail.com>
2022-11-18 23:05:16 +0900

    221118 피드백

--- backend/services/PostService.js
+++ backend/services/PostService.js
@@ -3,32 +3,43 @@ const PostModel = require("../models/Post");
 
 // 전체 게시글 조회
 exports.getAllPosts = async () => {
-  return await PostModel.find();
+  const result = await PostModel.find();
+  return result;
 };
 
 // 게시글 생성
 exports.createPost = async (post) => {
-  return await PostModel.create(post);
+  const result = await PostModel.create(post);
+  return result;
 };
 
 // 특정 게시글 조회
 exports.getPostById = async (postId) => {
   const _id = new mongoose.Types.ObjectId(postId);
-  return await PostModel.findOne({
+  const result = await PostModel.findOne({
     _id: _id,
   });
+  return result;
   // .populate("name")
 };
 
 // 게시글 수정
-exports.updatePost = async (postId) => {
-  const _id = new mongoose.Types.ObjectId(postId);
-  return await PostModel.findByIdAndUpdate({
-    _id: _id,
-  });
+exports.updatePost = async (postId, body) => {
+  // const _id = new mongoose.Types.ObjectId(postId);
+  // Model.findByIdAndUpdate(id, { name: 'jason bourne' }, options, callback)
+  const result = await PostModel.findByIdAndUpdate(
+    postId,
+    {
+      title: body.title,
+      contents: body.contents,
+    },
+    { new: true }
+  );
+  return result;
 };
 
 // 게시글 삭제
 exports.deletePost = async (postId) => {
-  return await PostModel.findByIdAndDelete(postId);
+  await PostModel.findByIdAndDelete(postId);
+  return;
 };
